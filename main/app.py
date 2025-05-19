9ec80bea25f79becd9715f74ec6d142da4d8724e
Leonardo Takeshi <takeshileonardo.1@gmail.com>
2025-05-19 17:01:05 -0400

    Update app.py

--- main/app.py
+++ main/app.py
@@ -5,12 +5,10 @@ import os
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-CORS(app) #habilita o cors e permite requisições no front
+CORS(app)  # habilita o cors e permite requisições no front
 
-#config para upload de arquivos, ainda opcional
+# config para upload de arquivos, ainda opcional
 UPLOAD_FOLDER = 'uploads'
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-
