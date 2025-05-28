641305726ede1a0fcc72c995e127582a020afa71
Leonardo Takeshi <takeshileonardo.1@gmail.com>
2025-05-28 10:45:16 -0400

    formatado

--- backend/controladores/Classes.py
+++ backend/controladores/Classes.py
@@ -4,27 +4,12 @@ from openpyxl.utils.dataframe import dataframe_to_rows
 from openpyxl.styles import numbers
 from openpyxl import load_workbook
 import openpyxl
-from controladores.troca_data import DataCorreta
 
 
 class ProcessaDados:
     def __init__(self, csv_path, excel_path):
         self.csv_df = pd.read_csv(csv_path, sep=";")
         self.excel_df = pd.read_excel(excel_path)
-
-        # #CORRIGE AS DATAS ANTES DO COMPARATIVO
-        # corretor = DataCorreta()
-        # if 'DATA_CONTRATO' in self.excel_df.columns:
-        #     self.excel_df = corretor.corrigir_coluna(self.excel_df, 'DATA_CONTRATO')
-
-        # if 'DATA_ADESAO' in self.excel_df.columns:
-        #     self.excel_df = corretor.corrigir_coluna(self.excel_df, 'DATA_ADESAO')
-
-        # Converter datas
-        # if 'Data de criação' in self.excel_df.columns:
-        #self.csv_df['Data de criação'] = pd.to_datetime(self.csv_df['Data de criação'], dayfirst=True,errors='coerce')
-        # self.excel_df['DATA_CONTRATO'] = pd.to_datetime(self.excel_df['DATA_CONTRATO'], dayfirst=True, errors='coerce')
-        # self.excel_df['DATA_ADESAO'] = pd.to_datetime(self.excel_df['DATA_ADESAO'], dayfirst=True, errors='coerce')
 
         # Filtrar por mês atual
         self.filtrar_mes_atual()
