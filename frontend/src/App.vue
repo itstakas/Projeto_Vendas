4449cb0e9b2f84b5c79f616e3fe2880107c80be0
Leonardo Takeshi <takeshileonardo.1@gmail.com>
2025-06-04 17:01:01 -0400

    Começo da interface, dificuldades em integração com back

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 
-import EnviarArquivos from './components/EnviarArquivos.vue';
+import EnviarCsvExcel from './components/EnviarCsvExcel.vue';
 
 </script>
 
 <template>
 
-<EnviarArquivos/>
+<EnviarCsvExcel/>
 
 </template>
---
+++ frontend/src/components/EnviarCsvExcel.vue
@@ -0,0 +1,53 @@
+
+<template>
+    <div class="container">
+        <h1>Upload de arquivos: </h1>
+        <button @click="triggerCsvUpload">Enviar CSV</button>
+        <button @click="triggerExcelUpload">Enviar EXCEL</button>
+        <p>
+            <button type="submit">Enviar</button>
+        </p>
+
+    <input 
+        type="file"
+        ref="csvInput"
+        accept=".csv"
+        @change="handleCsvFile"
+        style="display: none;"
+    />
+
+    <input 
+        type="file"
+        ref="excelInput"
+        accept=".xlsx, .xls"
+        @change="handleExcelFile"
+        style="display: none;"
+    />
+    </div>
+    <EnviarArquivos/>
+</template>
+
+<script setup>
+import EnviarArquivos from './EnviarArquivos.vue';
+import { ref } from 'vue';
+
+const csvInput = ref(null)
+const excelInput = ref(null)
+
+const triggerCsvUpload = ()=>{
+    csvInput.value.click(); 
+}
+
+const triggerExcelUpload = ()=>{
+    excelInput.value.click();
+}
+
+const handleCsvFile = (event)=>{
+    const file = event.target.files[0];
+}
+
+const handleExcelFile = (event)=>{
+    const file = event.target.file[0]
+}
+
+</script>
