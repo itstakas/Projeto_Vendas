8be7b8a4c19b2582b0ae71a04f9c1b990867129d
Leonardo Takeshi <takeshileonardo.1@gmail.com>
2025-06-28 08:10:23 -0400

    Update deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './', // 👈 faz os caminhos dos assets serem relativos
+  build: {
+    outDir: 'dist' // (opcional, já é padrão)
+  }
 })
